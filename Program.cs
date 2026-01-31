a6612063f438df75ba718d406cc8f036df28f546
Admin marcoes.net <admin@marcoes.net>
2026-01-31 16:40:55 +0000

    Disable HTTPS Redirection

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
 app.UseAntiforgery();
 
